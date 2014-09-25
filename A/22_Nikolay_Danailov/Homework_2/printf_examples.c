2c517256f8e31737615c9ce301707437dc427ee9
Nikolay <frostblooded@yahoo.com>
2014-09-25 09:20:02 +0300

    Made printing of random double

--- A/22_Nikolay_Danailov/Homework_2/printf_examples.c
+++ A/22_Nikolay_Danailov/Homework_2/printf_examples.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <time.h>
+const int RandMax = 2000000000;
+
+double randfrom(double min, double max) 
+{
+	srand(time(NULL));
+    	double range = (max - min); 
+    	double divider = RandMax / range;
+    	return min + (rand() / divider);
+}
 
 int main()
 {
-	srand(time(NULL));
-	int randomInt = rand();
-	printf("Random integer number: %d\n", randomInt);
+	printf("Random integer number: %lf\n", randfrom(1l, RandMax));
 	return 0;
 }
+
