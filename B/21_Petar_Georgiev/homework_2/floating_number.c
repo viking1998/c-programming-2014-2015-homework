5e71f400e27d481306b64dd60c30c39c1c7408a2
SWAGSLAYER <pepi2401_98@abv.bg>
2014-09-27 13:11:01 -0700

    Update and rename integer.c to floating_number.c

---
+++ B/21_Petar_Georgiev/homework_2/floating_number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+float a;
+a = 420.69f;
+printf("random number = %5.2f\n" a);
+return 0;
+}
--- B/21_Petar_Georgiev/homework_2/integer.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-int a;
-a = 21;
-printf("random number = %d\n" a);
-return 0;
-}
