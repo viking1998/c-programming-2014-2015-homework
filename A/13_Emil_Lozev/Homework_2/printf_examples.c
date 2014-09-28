5393e5c02f98d776f265bbb9e8ff8649c26fc8e5
emchoko <emilfrogy@gmail.com>
2014-09-28 13:56:10 +0300

    Added float and double var

--- A/13_Emil_Lozev/Homework_2/printf_examples.c
+++ A/13_Emil_Lozev/Homework_2/printf_examples.c
@@ -2,8 +2,10 @@
 
  int main()
 {
- int number;
- number = 6;
- printf("Number = %d\n", number);
+ float float_n;
+ double double_n;
+	float_n = 1.24498;
+	double_n = 5.3562;
+ printf("Float = %.2f\nDouble = %.3lf\n", float_n, double_n);
  return 0;
 }
