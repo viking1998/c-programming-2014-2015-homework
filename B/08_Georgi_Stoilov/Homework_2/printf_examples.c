9e81e8374f7b7a7c2be044d847de493fd7992bfa
georgistoilov8 <gogo.stoilov@abv.bg>
2014-09-28 23:31:31 +0300

    Change to char

--- B/08_Georgi_Stoilov/Homework_2/printf_examples.c
+++ B/08_Georgi_Stoilov/Homework_2/printf_examples.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main() {
-float float_num = 50.9547f;
-float num_float = 18.246785f;
-	printf("Float_Number = %04.3f\n Float_Number2 = %04.3f\n", float_num, num_float);
+char a = 'L';
+char b = 'A';
+char c = 'S';
+char d = 'T';
+
+	printf(" %c %c %c %c\n", a, b, c, d);
 	return 0;
 
 }
