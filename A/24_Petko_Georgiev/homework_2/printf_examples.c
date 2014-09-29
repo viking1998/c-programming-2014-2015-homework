fa3dabde66f48a2f04fa96cc3ecf82e36f8e964b
bassist666 <Petko_Vas_Georgiev@abv.bg>
2014-09-29 10:43:21 +0300

    Print some characters

--- A/24_Petko_Georgiev/homework_2/printf_examples.c
+++ A/24_Petko_Georgiev/homework_2/printf_examples.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	printf("%f\n",666.666666);
+	char c[14] = {'I',' ','H','a','t','e',' ','P','e','o','p','l','e','!'};
+	int i=0; while(i<14) { printf("%c",c[i]); i++;}
+	printf("\n");
 	return 0;
 }
