70f59d2011a9f833654293e1efdfff03490888f1
Nicky <newmast@abv.bg>
2014-09-29 21:21:51 +0300

    change 3

--- B/20_Nikolay_Karagyozov/Homework_2/1.c
+++ B/20_Nikolay_Karagyozov/Homework_2/1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(){
-	int _integer = 20;
+	float _float;
+	_float = 4.764365f;
 
-	printf("Cqlo chislo: %d\n", _integer);
+	printf("Chislo s plavashta zapetaq: %.3f\n", _float);
 return 0;
 }
