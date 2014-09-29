d89512ac2323d09277682f2fa6e44ec05456b07a
bufferies <pavel1998@abv.bg>
2014-09-29 20:32:16 +0300

    Update printf_examples.c
    Added a character.

--- A/23_Pavel_Kostadinov/Homework_2/printf_examples.c
+++ A/23_Pavel_Kostadinov/Homework_2/printf_examples.c
@@ -6,7 +6,9 @@ int num;
 num=10;
 printf("Num=%d\n",num);
 float float_number=5,9f;
-printf("float=%f\n",float_number)
+printf("float=%f\n",float_number);
+char A=A;
+printf("Char=%c\n",A);
 return 0;
 }
   
