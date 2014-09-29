d4a9fde2092606fd8059a7b78290bc8617524863
Teodor-Dobrev <teodor_dobrew@abv.bg>
2014-09-29 20:51:52 +0300

    printf_examples.c

--- A/27_Teodor_Dobrev/Homework_2/printf_examples.c
+++ A/27_Teodor_Dobrev/Homework_2/printf_examples.c
@@ -2,5 +2,39 @@
 
 int main()
 {
-	printf("Hello world !\n");
+	int a;
+	float b;
+	char c,d,e,f,g,h,i,j,k,l,m,n;
+
+	a=2147483647;
+	b=98765.43210;
+	
+	c='H';
+	d='e';
+	e='l';
+	f='l';
+	g='o';
+	h=' ';
+	i='w';
+	j='o';
+	k='r';
+	l='l';
+	m='d';
+	n='!';
+
+	printf("%i\n",a);
+	printf("%f\n",b);
+
+	printf("%c",c);
+	printf("%c",d);
+	printf("%c",e);
+	printf("%c",f);
+	printf("%c",g);
+	printf("%c",h);
+	printf("%c",i);
+	printf("%c",j);
+	printf("%c",k);
+	printf("%c",l);
+	printf("%c",m);
+	printf("%c\n",n);
 }
