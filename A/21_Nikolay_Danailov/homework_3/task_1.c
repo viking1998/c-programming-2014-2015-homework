535e794ad7e86fd213b57e0ccf081252cee70a99
Nikolay <frostblooded@yahoo.com>
2014-10-09 19:17:56 +0300

    Made printing of characters from ASCII

--- A/21_Nikolay_Danailov/homework_3/task_1.c
+++ A/21_Nikolay_Danailov/homework_3/task_1.c
@@ -9,8 +9,28 @@ void PrintSmallLetters()
 	}
 }
 
+void PrintBigLetters()
+{
+	int i;
+	for (i = 65; i <= 90; i += 1)
+	{
+		printf("%c - %d\n", i, i);
+	}
+}
+
+void PrintDigits()
+{
+	int i;
+	for (i = 48; i <= 57; i += 1)
+	{
+		printf("%c - %d\n", i, i);
+	}
+}
+
 int main()
 {
 	PrintSmallLetters();
+	PrintBigLetters();
+	PrintDigits();
 	return 0;
 }
