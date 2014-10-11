02f9a9b8f7baf5bc0e6282e9638edbd6308f6eb5
velioo <velioocs@mail.bg>
2014-10-11 16:34:59 +0300

    added task_1.c

--- A/06_Velin_Yavorski/homework_3/task_1.c
+++ A/06_Velin_Yavorski/homework_3/task_1.c
@@ -4,10 +4,22 @@ int main()
 {
 char ch;
 int i;
+	ch=96;
+	for(i=97;i<=122;i++)
+	{	
+		ch++;
+		printf("%c - %d\n",ch,i);	
+	}
 	ch=64;
 	for(i=65;i<=90;i++){
-	ch++;
-	printf("%c - %d\n",ch,i);
+		ch++;
+		printf("%c - %d\n",ch,i);
+	}
+	ch=47;
+	for(i=48;i<=57;i++)
+	{
+		ch++;
+		printf("%c - %d\n",ch,i);
 	}
 return 0;
 }
