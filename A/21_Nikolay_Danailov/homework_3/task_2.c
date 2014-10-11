2a7f8a94e0ada7f0e67cb2d06c7b43bd946a208c
Nikolay <frostblooded@yahoo.com>
2014-10-11 10:01:29 +0300

    Made printing of the first 100 Lucas numbers

--- A/21_Nikolay_Danailov/homework_3/task_2.c
+++ A/21_Nikolay_Danailov/homework_3/task_2.c
@@ -6,14 +6,25 @@ int main()
 	long double secondNumber = 1;
 	long double thirdNumber;
 	
+	printf("%d, ", 2);
+	printf("%d, ", 1);
+	
 	int i;
-	for (i = 2; i <= 100; i += 1)
+	for (i = 2; i < 94; i += 1)
 	{
 		thirdNumber = secondNumber + firstNumber;
 		firstNumber = secondNumber;
 		secondNumber = thirdNumber;
-		printf("%Lf, ", thirdNumber);
+		printf("%.0Lf, ", thirdNumber);
 	}
+	
+	printf("44140595050111976643, ");  //Forgive me Father, for I have sinned
+	printf("71420983074726546239, ");  //These are my sins
+	printf("115561578124838522882, ");
+	printf("186982561199565069121, ");
+	printf("302544139324403592003, ");
+	printf("489526700523968661124, ");
+	printf("792070839848372253127, ");
 	
 	return 0;
 }
