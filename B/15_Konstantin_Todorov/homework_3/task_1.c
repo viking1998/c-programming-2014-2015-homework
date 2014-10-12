50d07868a1c1bf0ca9455ba608d57cb333c29121
Rahovec <konstantin.todorov98@Gmail.com>
2014-10-12 20:53:20 +0300

    task_1.c
    Konstantin Todorov 10B No15

---
+++ B/15_Konstantin_Todorov/homework_3/task_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+int i;
+int o;
+int k;
+i=97;
+//malki bukvi;
+do
+{ printf("%d - %c \n",i,i);
+i++;
+}
+while (i<=122);
+// golemi bukvi;
+o=65;
+do
+{ printf("%d - %c \n",o,o);
+o++;
+}
+while (o<=90);
+//cifri;
+k=48;
+do
+{ printf("%d - %c \n",k,k);
+k++;
+}
+while (k<=57);
+
+return 0;
+
+}
