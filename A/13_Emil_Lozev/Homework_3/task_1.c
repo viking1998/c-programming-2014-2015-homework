580b579c57e635bb21ee3601fd73de96d9f375d1
emchoko <emilfrogy@gmail.com>
2014-10-14 00:07:06 +0300

    Added task 1

---
+++ A/13_Emil_Lozev/Homework_3/task_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+ int i;
+  for(i = 97; i <= 122; i++){
+  printf("%c - %d\n", i,i);
+  }
+  for(i = 65; i <=90 ; i++){
+  printf("%c - %d\n", i,i);
+  }
+  for(i = 1; i <= 9; i++){
+  printf("%d -%d\n", i,i);
+  }
+ return 0;
+}
