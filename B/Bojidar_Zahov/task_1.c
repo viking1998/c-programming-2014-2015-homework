877f80e39440419e389bf7998920cac6f1d0b8f3
Bojidar <bzahov98@abv.bg>
2014-10-14 00:11:44 +0300

    Homework3

---
+++ B/Bojidar_Zahov/task_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+int i;
+char ch;
+   for(i = 97; i <= 122; i++){
+   ch =i;
+   printf("%c", ch);
+   printf(" - ");
+   printf("%d\n",i);
+   }
+   for(i = 65; i <= 90; i++){
+   ch =i;
+   printf("%c", ch);
+   printf(" - ");
+   printf("%d\n",i);
+}
+   for(i = 48; i <= 57; i++){
+   ch =i;
+   printf("%c", ch);
+   printf(" - ");
+   printf("%d\n",i);
+}
+}
